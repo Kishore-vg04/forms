d8090725386cbcbaf7cc342334799a4e651bc11e
Kishore-vg04 <165400951+Kishore-vg04@users.noreply.github.com>
2025-02-21 19:57:35 +0530

    Add files via upload

--- coordinator.js
+++ coordinator.js
@@ -3,13 +3,22 @@ function update(event) {
 
     if (card) {
         var remark = card.querySelector(".remark");
-        var input = card.querySelector(".inputs");
+        var inputs = card.querySelector(".inputs");
         var button = card.querySelector(".submit-disapproval");
         var approve = card.querySelector(".approve");
+        if(remark.style.display == "inline" && inputs.style.display == "inline"
+            && button.style.display == "inline" && approve.style.display == "none"){
+            remark.style.display = "none";
+            inputs.style.display = "none";
+            button.style.display = "none";
+            approve.style.display = "inline";
+            }
+            else{
+                remark.style.display = "inline";
+                inputs.style.display = "inline";
+                button.style.display = "inline";
+                approve.style.display = "none";
+            }
         
-        remark.style.display = "inline";
-        input.style.display = "inline";
-        button.style.display = "inline";
-        approve.style.display = "none";
     }
 }
--- hod.js
+++ hod.js
@@ -1,1 +1,4 @@
+var s1="Heollooo school are good";
+var s2=/\w{3}/g;
+console.log(s1.match(s2));
 
